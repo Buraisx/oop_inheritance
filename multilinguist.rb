c1b09cd7efe3d6261046fc89f5faaa9b62b70584
Situ William <situ_william@yahoo.com>
2017-12-19 23:24:46 -0500

    Test for invalid country not working

--- multilinguist.rb
+++ multilinguist.rb
@@ -18,17 +18,24 @@ class Multilinguist
   def initialize
     @current_lang = 'en'
   end
-
+  def not_found
+      {"status":404,"message":"Not Found"}
+  end
   # Uses the RestCountries API to look up one of the languages
   # spoken in a given country
   #
   # @param country_name [String] The full name of a country
   # @return [String] A 2 letter iso639_1 language code
   def language_in(country_name)
-    params = {query: {fullText: 'true'}}
-    response = HTTParty.get("#{COUNTRIES_BASE_URL}/#{country_name}", params)
-    json_response = JSON.parse(response.body)
-    json_response.first['languages'].first['iso639_1']
+
+      params = {query: {fullText: 'true'}}
+      response = HTTParty.get("#{COUNTRIES_BASE_URL}/#{country_name}", params)
+    if !country_name.empty?
+      json_response = JSON.parse(response.body)
+      json_response.first['languages'].first['iso639_1']
+    else
+      return "NA"
+    end
   end
 
   # Sets @current_lang to one of the languages spoken
@@ -77,19 +84,19 @@ class QuoteCollector < Multilinguist
   end
 end
 
-me = MathGenius.new
-puts me.report_total([23,45,676,34,5778,4,23,5465]) # The total is 12048
-me.travel_to("India")
-puts me.report_total([6,3,6,68,455,4,467,57,4,534]) # है को कुल 1604
-me.travel_to("Italy")
-puts me.report_total([324,245,6,343647,686545]) # È Il totale 1030767
-
-me2 = QuoteCollector.new
-me2.add_quote("90 percent of facts on the internet are true - Genghis Khan")
-me2.add_quote("Fly you fools - Dumbledore")
-me2.add_quote("Why don't you take a seat over here? - Chris Handsome")
-puts me2.unleash_quote
-me2.travel_to("Japan")
-puts me2.unleash_quote
-me2.travel_to("Spain")
-puts me2.unleash_quote
+# me = MathGenius.new
+# puts me.report_total([23,45,676,34,5778,4,23,5465]) # The total is 12048
+# me.travel_to("India")
+# puts me.report_total([6,3,6,68,455,4,467,57,4,534]) # है को कुल 1604
+# me.travel_to("Italy")
+# puts me.report_total([324,245,6,343647,686545]) # È Il totale 1030767
+#
+# me2 = QuoteCollector.new
+# me2.add_quote("90 percent of facts on the internet are true - Genghis Khan")
+# me2.add_quote("Fly you fools - Dumbledore")
+# me2.add_quote("Why don't you take a seat over here? - Chris Handsome")
+# puts me2.unleash_quote
+# me2.travel_to("Japan")
+# puts me2.unleash_quote
+# me2.travel_to("Spain")
+# puts me2.unleash_quote
---
+++ test_multilinguist.rb
@@ -0,0 +1,18 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './multilinguist.rb'
+
+class TestMultilinguist < MiniTest::Test
+
+  def setup
+    @speaker = Multilinguist.new
+
+  end
+  def test_language_in_no_country
+    assert_equal("NA"eaker.language_in(""))
+  end
+
+  def test_language_in_appropriate_country
+    assert_equal("hi",@speaker.language_in("india"))
+  end
+end
